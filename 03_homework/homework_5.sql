f65138d1fcf020267f1892acaf57e7ba963012b8
cavengal <125897415+cavengal@users.noreply.github.com>
2024-05-25 19:32:16 -0400

    Submission of homework 5

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -1,33 +1,3 @@
--- String manipulations
-/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
-These are separated from the product name with a hyphen. 
-Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
-Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
-
-| product_name               | description |
-|----------------------------|-------------|
-| Habanero Peppers - Organic | Organic     |
-
-Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
-
-
-
-/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
-
-
-
--- UNION
-/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
-
-HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
-1) Create a CTE/Temp Table to find sales values grouped dates; 
-2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
-"best day" and "worst day"; 
-3) Query the second temp table twice, once for the best day, once for the worst day, 
-with a UNION binding them. */
-
-
-
 -- Cross Join
 /*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
 customer on record. How much money would each vendor make per product? 
@@ -39,7 +9,22 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
+WITH scenario AS(
+	SELECT DISTINCT vendor_id, product_id, original_price, customer_id, 5 AS qty_sold_per_customer
+	FROM vendor_inventory AS vi
+	CROSS JOIN customer
+)
+SELECT
+vendor_name,
+product_name,
+SUM(qty_sold_per_customer*original_price) AS scenario_sales_per_product
 
+
+FROM scenario
+	LEFT JOIN vendor AS v ON scenario.vendor_id=v.vendor_id
+	LEFT JOIN product AS p ON scenario.product_id=p.product_id
+	
+GROUP BY vendor_name, product_name
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -47,11 +32,18 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
+DROP TABLE IF EXISTS product_units;
+CREATE TABLE product_units AS
+	SELECT *,CURRENT_TIMESTAMP AS snapshot_timestamp
+	FROM product
+	WHERE product_qty_type = 'unit';
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
+INSERT INTO product_units
+VALUES (23,'Maple Syrup - Jar', '10 oz',2,'unit',CURRENT_TIMESTAMP);
 
 
 -- DELETE
@@ -59,7 +51,14 @@ This can be any product you desire (e.g. add another record for Apple Pie). */
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
-
+DELETE FROM product_units
+WHERE 
+	product_id=23
+	AND snapshot_timestamp =(
+		SELECT MIN(snapshot_timestamp)
+		FROM product_units
+		WHERE product_id=23
+	);
 
 -- UPDATE
 /* 1.We want to add the current_quantity to the product_units table. 
@@ -78,4 +77,28 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
+ALTER TABLE product_units
+ADD current_quantity INT;
 
+UPDATE product_units
+SET current_quantity =  COALESCE((
+    SELECT vi.latest_inventory
+    FROM (
+		SELECT
+		product_id,
+		SUM(quantity) AS latest_inventory
+		-- The Subquery brings the sum at the product level of the quantities reported by each vendor.
+		-- Eventhough in our database, one product is not sold by two different vendors
+		-- this Subquery contemplates that possibility
+		FROM  (SELECT
+					market_date,
+					vendor_id,
+					product_id,
+					quantity,
+					ROW_NUMBER() OVER (PARTITION BY vendor_id, product_id ORDER BY market_date DESC) AS latest_quantity_record_by_vendor
+				FROM vendor_inventory) AS latest_report
+				WHERE latest_quantity_record_by_vendor=1
+				GROUP BY product_id
+				) AS vi
+    WHERE vi.product_id = product_units.product_id
+),0);
